63940d520be722b57c050253cca76cffce72c19b
skmcloughlin <skmcloughlin@gmail.com>
2020-03-31 14:17:28 -0400

    adding onChange events

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ANIMALS } from '@frontendmasters/pet'
 
 const SearchParams = () => {
-    const location = "Seattle, WA"
-
-
+    const [location, setLocation] = useState('Seattle, Wa')
+    const [animal, setAnimal] = useState('dog')
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="locaton">
                     Location
-                 <input id="location" value={location} placeholder="location" />
+                 <input
+                        id="location"
+                        value={location}
+                        placeholder="location"
+                        onChange={e => setLocation(e.target.value)}/>
+                </label>
+                <label htmlFor="animal">Animal
+                     <select
+                        id="animal"
+                        value={animal}
+                        onChange={e => setAnimal(e.target.value)}></select>
                 </label>
                 <button>Submit</button>
             </form>
