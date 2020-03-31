54fba6d81a94a1cf604cfcecf5289c42e8736ccc
skmcloughlin <skmcloughlin@gmail.com>
2020-03-31 14:17:03 -0400

    adding onChange events

---
+++ src/SearchParams.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SearchParams = () => {
+    const location = "Seattle, WA"
+
+
+    return (
+        <div className="search-params">
+            <form>
+                <label htmlFor="locaton">
+                    Location
+                 <input id="location" value={location} placeholder="location" />
+                </label>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchParams
