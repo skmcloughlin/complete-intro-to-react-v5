aa4f4305f8c2cdb1c3cdb86b8d483413971ec3fd
skmcloughlin <skmcloughlin@gmail.com>
2020-03-31 13:30:02 -0400

    configured ESLint for React

--- src/Pet.js
+++ src/Pet.js
@@ -1,8 +1,15 @@
 import React from 'react';
 export default function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, props.name),
+//     React.createElement("h2", {}, props.animal),
+//     React.createElement("h2", {}, props.breed),
+//   ]);
+    return (
+        <div>
+            <h1>{name}</h1>
+            <h2>{animal}</h2>
+            <h2>{breed}</h2>
+        </div>
+    )
 };
