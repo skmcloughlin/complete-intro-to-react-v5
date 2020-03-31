1a40af2f3a3045f49300fe69d3adf03a7be1352f
skmcloughlin <skmcloughlin@gmail.com>
2020-03-31 14:53:21 -0400

    added animal dropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -4,27 +4,52 @@ import { ANIMALS } from '@frontendmasters/pet'
 const SearchParams = () => {
     const [location, setLocation] = useState('Seattle, Wa')
     const [animal, setAnimal] = useState('dog')
+    const [breed, setBreed] = useState('')
+    const [breeds, setBreeds] = useState([])
+    
     return (
-        <div className="search-params">
-            <form>
-                <label htmlFor="locaton">
-                    Location
-                 <input
-                        id="location"
-                        value={location}
-                        placeholder="location"
-                        onChange={e => setLocation(e.target.value)}/>
+      <div className="search-params">
+        <form>
+          <label htmlFor="locaton">
+            Location
+            <input
+              id="location"
+              value={location}
+              placeholder="location"
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </label>
+          <label htmlFor="animal">
+            Animal
+            <select
+              id="animal"
+              value={animal}
+              onChange={(e) => setAnimal(e.target.value)}
+              onBlur={(e) => setAnimal(e.target.value)}>
+              <option>All</option>
+              {ANIMALS.map((animal) => (
+                  <option key={animal} value={animal}>
+                      {animal}
+                  </option>
+              ))}
+            </select>
                 </label>
-                <label htmlFor="animal">Animal
+                <label htmlFor="breed">
+                    Breed
                      <select
-                        id="animal"
-                        value={animal}
-                        onChange={e => setAnimal(e.target.value)}></select>
+                        id="breed"
+                        value={breed}
+                        onChange={e => setBreed(e.target.value)}
+                        onBlur={e => setBreed(e.target.value)}
+                        disabled={breeds.length === 0}>
+                        <option>All</option>
+                        {breeds.map(breedString => <option key={breedString} value = {breedString}>{breedString}</option>)}
+                     </select>
                 </label>
-                <button>Submit</button>
-            </form>
-        </div>
-    )
+          <button>Submit</button>
+        </form>
+      </div>
+    );
 }
 
 export default SearchParams
