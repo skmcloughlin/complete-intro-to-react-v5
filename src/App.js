c54dc163ed375de6aceb7b2974b9909e433e5cf9
skmcloughlin <skmcloughlin@gmail.com>
2020-04-01 13:38:48 -0400

    added a theme context using hooks approach

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { render } from 'react-dom'
 import {Router, Link} from '@reach/router'
 import SearchParams from './SearchParams'
 import Details from './Details'
-
+import ThemeContext from './ThemeContext'
 
 const App = () => {
-
+  const themeHook = useState("darkblue")
   return (
     <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/"> Adopt Me! </Link>
-        </header>
-        <Router>
-          <Details path="details/:id"></Details>
-          <SearchParams path="/" />
-        </Router>
-      </div>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/"> Adopt Me! </Link>
+          </header>
+          <Router>
+            <Details path="details/:id"></Details>
+            <SearchParams path="/" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
